26923213a0199742fe7818499b63e501629e1e47
Justin Faulk <j.faulk@me.com>
2024-09-19 16:54:24 -0400

    Bump patch version to 1.1.11

--- network_scanner/network_scanner/settings.py
+++ network_scanner/network_scanner/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Use WhiteNoise for serving static files when running as a standalone app
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
