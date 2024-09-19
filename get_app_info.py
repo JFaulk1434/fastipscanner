746a093979df863dfab9f09e48a04a15c1f47c1b
Justin Faulk <j.faulk@me.com>
2024-09-19 17:06:11 -0400

    Bump patch version to 1.1.13

--- get_app_info.py
+++ get_app_info.py
@@ -5,27 +5,22 @@ print(f"Current working directory: {os.getcwd()}")
 print(f"Python path: {sys.path}")
 
 project_root = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, project_root)
-print(f"Added to Python path: {project_root}")
+network_scanner_path = os.path.join(project_root, "network_scanner")
+sys.path.insert(0, network_scanner_path)
+print(f"Added to Python path: {network_scanner_path}")
 
 try:
-    from network_scanner.network_scanner.app_info import (
-        APP_NAME,
-        APP_VERSION,
-        APP_EXECUTABLE_NAME,
-    )
+    from network_scanner.app_info import APP_NAME, APP_VERSION
 
-    print(
-        f"Successfully imported APP_NAME: {APP_NAME}, APP_VERSION: {APP_VERSION}, APP_EXECUTABLE_NAME: {APP_EXECUTABLE_NAME}"
-    )
+    print(f"Successfully imported APP_NAME: {APP_NAME}, APP_VERSION: {APP_VERSION}")
 except ImportError as e:
     print(f"Error: Could not import app_info. Details: {e}", file=sys.stderr)
-    print("Contents of current directory:")
-    for root, dirs, files in os.walk("."):
+    print("Contents of network_scanner directory:")
+    for root, dirs, files in os.walk(network_scanner_path):
         for file in files:
             print(os.path.join(root, file))
     sys.exit(1)
 
 print(
-    f'/DMyAppName="{APP_NAME}" /DMyAppVersion="{APP_VERSION}" /DMyAppExeName="{APP_EXECUTABLE_NAME}.exe"'
+    f'/DMyAppName="{APP_NAME}" /DMyAppVersion="{APP_VERSION}" /DMyAppExeName="{APP_NAME}.exe"'
 )
