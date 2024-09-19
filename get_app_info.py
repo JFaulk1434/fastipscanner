25a3e1d778bfaaa3723124d18f6ee2402d8561eb
Justin Faulk <j.faulk@me.com>
2024-09-19 16:06:24 -0400

    Bump patch version to 1.1.5

--- get_app_info.py
+++ get_app_info.py
@@ -6,14 +6,14 @@ project_root = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, project_root)
 
 try:
-    from network_scanner.network_scanner.settings import APP_NAME, VERSION
+    from network_scanner.network_scanner.app_info import APP_NAME, APP_VERSION
 except ImportError:
     print(
-        "Error: Could not import settings. Make sure the path is correct.",
+        "Error: Could not import app_info. Make sure the path is correct.",
         file=sys.stderr,
     )
     sys.exit(1)
 
 print(
-    f'/DMyAppName="{APP_NAME}" /DMyAppVersion="{VERSION}" /DMyAppExeName="{APP_NAME}.exe"'
+    f'/DMyAppName="{APP_NAME}" /DMyAppVersion="{APP_VERSION}" /DMyAppExeName="{APP_NAME}.exe"'
 )
