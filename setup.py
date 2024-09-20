d84c6311bf860272a19566e2cf34d361a51d631d
Justin Faulk <j.faulk@me.com>
2024-09-20 12:36:27 -0400

    Fixed name for pypi

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ import os
 from setuptools import setup, find_packages
 
 VERSION = "1.2.5"
-APP_NAME = "Fast IP Scanner"
+APP_NAME = "fast_ip_scanner"
+APP_DISPLAY_NAME = "Fast IP Scanner"
 DESCRIPTION = "A clean, user-friendly web interface for fast IP scanning, port scanning, and network analysis. Built with Django and Scapy, Fast IP Scanner provides powerful network tools with an intuitive UI."
-SITE_HEADER = APP_NAME
+SITE_HEADER = APP_DISPLAY_NAME
 AUTHOR = "Justin Faulk"
 AUTHOR_EMAIL = "deww1434@gmail.com"
 URL = "https://github.com/JFaulk1434/fastipscanner"
