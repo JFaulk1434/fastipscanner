66a0a44bcdd9aa7de28a607c96e13ba5ee4207b7
Justin Faulk <j.faulk@me.com>
2024-09-27 14:55:01 -0400

    fixed page name

--- fastipscanner/fastipscanner/context_processors.py
+++ fastipscanner/fastipscanner/context_processors.py
@@ -1,8 +1,8 @@
-from .app_info import APP_NAME, VERSION
+from .app_info import APP_DISPLAY_NAME, VERSION
 
 
 def app_info(request):
     return {
-        "APP_NAME": APP_NAME,
+        "APP_NAME": APP_DISPLAY_NAME,
         "VERSION": VERSION,
     }
